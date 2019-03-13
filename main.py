0fa51f4d5a26c53ebc342e3676174eedac5de967
Tomas Michalek <tomas.michalek.st@vsb.cz>
2019-03-13 18:58:38 +0100

    `Modify`: Code cleanup
    Cleanup of code to make it runnable on PC smoother.

--- main.py
+++ main.py
@@ -1,47 +1,36 @@
-import Turtle
+import turtle
 
-color="#FF0022"
-speed=0
 angle=60
 size=50
 
-def one_side(pen):
-  retroAngle = angle /2
-      
-  pen.forward(size/3)
-  pen.right(retroAngle)
-  pen.forward(size/6)
-  pen.right(-retroAngle*2)
-  pen.forward(size/6)
-  pen.right(retroAngle)
-  pen.forward(size/3)
-  
-  pen.right(angle)
+turtle.speed(0)
+turtle.tracer(100)
+turtle.hideturtle()
+turtle.color("#FF0022")
+
+def one_side():
+  turtle.forward(size/3)
+  turtle.right(angle /2)
+  turtle.forward(size/6)
+  turtle.right(-angle)
+  turtle.forward(size/6)
+  turtle.right(angle /2)
+  turtle.forward(size/3)
+  turtle.right(angle)
     
-def patvar(pen):
+def patvar():
   for _ in range(0, 36):
     for i in range(0, 6):
-      one_side(pen)
+      one_side()
      
     retroAngle = 105 
-    pen.right(-retroAngle)
-    pen.back(size/2 + 10)
-    pen.forward(size/2 + 10)
-    pen.right(retroAngle)
+    turtle.right(-retroAngle)
+    turtle.back(size/2 + 10)
+    turtle.forward(size/2 + 10)
+    turtle.right(retroAngle)
       
-    pen.forward(size)
-    pen.right(angle/2)
+    turtle.forward(size)
+    turtle.right(angle/2)
   
-
-myPen = turtle.Turtle()
-myPen.speed(speed)
-myPen.color(color)
-
-myPen.penup()
-myPen.goto(0,100) #position cursor at the bootom right of the screen
-myPen.pendown()
-
-patvar(myPen)
-#one_side(myPen)
-myPen.penup()
-myPen.goto(-100, -100)
+patvar()
+turtle.getscreen()._root.mainloop()
